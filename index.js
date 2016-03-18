e9548867962bae60bfda151c38252f6f3b037a24
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-03-18 05:17:57 +0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var insert = require('gulp-insert');
+
+var comment = function(bundlePath ) {
+    return insert.transform(function(contents, file) {
+        var filePath = bundlePath ? path.relative(bundlePath, file.path) : file.path,
+            commentsBegin = '/* ' + filePath + ':begin */\n',
+            commentsEnd = '\n/* ' + filePath + ':end */\n';
+        return commentsBegin + contents + commentsEnd;
+    });
+};
+
+module.exports = comment;
