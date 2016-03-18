38fbc3228351670c5779afd02bb3728d0d1f3160
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-03-18 20:25:44 +0300

    Separate js / css comments

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var path = require('path');
 var insert = require('gulp-insert');
 
-var comment = function(bundlePath ) {
+var cssComment = function(filePath, contents) {
+	var commentsBegin = '/* ' + filePath + ':begin */\n',
+		commentsEnd = '/* ' + filePath + ':end */';
+	return commentsBegin + contents + commentsEnd;
+}
+
+var jsComment = function(filePath, contents) {
+	var commentsBegin = '/* begin: ' + filePath + ' */\n',
+		commentsEnd = '\n/* end: ' + filePath + ' */';
+	return commentsBegin + contents + commentsEnd;
+}
+
+var comment = function(bundlePath, commentFn) {
     return insert.transform(function(contents, file) {
-        var filePath = bundlePath ? path.relative(bundlePath, file.path) : file.path,
-            commentsBegin = '/* ' + filePath + ':begin */\n',
-            commentsEnd = '/* ' + filePath + ':end */';
-        return commentsBegin + contents + commentsEnd;
+        var filePath = bundlePath ? path.relative(bundlePath, file.path) : file.path;
+		return commentFn(filePath, contents);
     });
 };
 
-module.exports = comment;
+module.exports = function(bundlePath) { return comment(bundlePath, cssComment); };
+module.exports.css = function(bundlePath) { return comment(bundlePath, cssComment); };
+module.exports.js  = function(bundlePath) { return comment(bundlePath, jsComment); };
