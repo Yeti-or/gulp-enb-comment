3b7d4ba7e6624199b1b786edc2da8e12d0344b8d
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-03-19 12:30:38 +0300

    Add more tests

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var gulpUtil = require('gulp-util');
 var enbComment = require('.');
 
@@ -8,13 +8,66 @@ it('should wrap content with comment', function(done) {
 	var stream = enbComment();
 
 	stream.on('data', function(file) {
-		console.log(file.contents.toString());
-		assert.notEqual(file.contents.toString(), content);
+		expect(file.contents.toString()).not.to.equal(content);
 	});
 	stream.on('end', done);
 
 	stream.write(new gulpUtil.File({
 		path: '/file.js',
+		contents: new Buffer(content)
+	}));
+
+	stream.end();
+});
+
+it('should add relative path to comment', function(done) {
+	var bundlePath = 'bundle/assets';
+	var stream = enbComment(bundlePath);
+
+	stream.on('data', function(file) {
+		var strings = file.contents.toString().split('\n');
+		expect(strings[0]).to.include('../file.js');
+	});
+	stream.on('end', done);
+
+	stream.write(new gulpUtil.File({
+		path: 'bundle/file.js',
+		contents: new Buffer(content)
+	}));
+
+	stream.end();
+});
+
+it('should have js-comments', function(done) {
+	var bundlePath = 'bundle/assets';
+	var stream = enbComment.js(bundlePath);
+
+	stream.on('data', function(file) {
+		var content = file.contents.toString();
+		expect(content).to.eql('/* begin: ../file.js */\nENB is not BEM\n/* end: ../file.js */');
+	});
+	stream.on('end', done);
+
+	stream.write(new gulpUtil.File({
+		path: 'bundle/file.js',
+		contents: new Buffer(content)
+	}));
+
+	stream.end();
+});
+
+it('should have css-comments', function(done) {
+	var bundlePath = 'bundle/assets';
+	var stream = enbComment.css(bundlePath);
+
+	stream.on('data', function(file) {
+		var content = file.contents.toString();
+		expect(content).to.eql('/* ../file.js:begin */\nENB is not BEM/* ../file.js:end */');
+	});
+	stream.on('end', done);
+
+	stream.write(new gulpUtil.File({
+		path: 'bundle/file.js',
 		contents: new Buffer(content)
 	}));
 
